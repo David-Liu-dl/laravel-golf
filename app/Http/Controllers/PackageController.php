db6ef47c2e9a53ddfc39a38ca125d1a834ab4d10
jing yang <ningyangjing@126.com>
2016-12-15 00:13:45 +1100

    uuu

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -1,17 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Package;
 use Jenssegers\Agent\Agent as Agent;
+use App\Order;
+use Mail;
+use Excel;
+use Carbon\Carbon;
 
 class PackageController extends Controller {
-    
+
     public function index($id) {
         $package = Package::find($id);
-        if(empty($package)){
+        if (empty($package)) {
             $package = $this->first();
         }
-        if(!empty($package)){
+        if (!empty($package)) {
             $package['features'] = explode(";", $package['features']);
         }
         return view('packages')->with("package", $package);
@@ -20,12 +25,12 @@ class PackageController extends Controller {
     public function getAll() {
         $packages = Package::all();
 
-        foreach ($packages as $package){
+        foreach ($packages as $package) {
 
-            if(empty($package)){
+            if (empty($package)) {
                 $package = $this->first();
             }
-            if(!empty($package)){
+            if (!empty($package)) {
                 $package['features'] = explode(";", $package['features']);
             }
         }
@@ -35,13 +40,12 @@ class PackageController extends Controller {
 
         if ($Agent->isMobile()) {
             return view('mobileViews.packages')->with("packages", $packages);
-        }else{
+        } else {
             return view('packages')->with("packages", $packages);
         }
-        
     }
-    
-    private function first(){
+
+    private function first() {
         $packet = array();
         $packet['title'] = "test";
         $packet['description'] = "test test test test test test test test ";
@@ -50,4 +54,37 @@ class PackageController extends Controller {
         $packet['price'] = 299.99;
         return $packet;
     }
+
+    public function email() {
+        $former_order = array();
+        $former_order['total'] = Order::all()->count();
+        $month = Carbon::now()->subMonth()->startOfMonth();
+        $seven_days = Carbon::now()->subDay(7)->startOfDay();
+        $yesterday_start = Carbon::yesterday()->startOfDay();
+        $yesterday_end = Carbon::yesterday()->endOfDay();
+        $former_order['month'] = Order::whereBetween('created_at', [$month, $yesterday_end])->count();
+        $former_order['7days'] = Order::whereBetween('created_at', [$seven_days, $yesterday_end])->count();
+        $orders = Order::whereBetween('created_at', [$yesterday_start, $yesterday_end])->get();
+        
+        //import excel
+        $cellData = array();
+        $cellData[0] = ['ID', 'Name', 'Phone', 'Hand', 'Email', 'Book Block', 'Booked At'];
+        if(!empty($orders)){
+            foreach($orders as $value){
+                array_push($cellData, [$value->id, $value->name, $value->phone, $value->hand, $value->email, $value->book_block, $value->created_at]);
+            }
+        }
+        Excel::create('Order_List_'.Carbon::yesterday()->toDateString(),function($excel) use ($cellData){
+            $excel->sheet('orders', function($sheet) use ($cellData){
+                $sheet->rows($cellData);
+            });
+        })->store('xls', storage_path('excel/exports'));
+        
+        Mail::send('orders', ['orders' => $orders, 'former_order' => $former_order, 'today' => Carbon::yesterday()->toDateString()], function($message) {
+            $to = 'euniceyangjing@gmail.com';
+            $message->to($to)->subject("Orders List for ".Carbon::yesterday()->toDateString());
+            $message->attach(storage_path('excel/exports/').'Order_List_'.Carbon::yesterday()->toDateString().'.xls');
+        });
+    }
+
 }
