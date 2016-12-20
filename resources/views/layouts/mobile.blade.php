00f87e7da4b4342689d7418366335ebd85e9b6ee
jing yang <ningyangjing@126.com>
2016-12-21 01:29:11 +1100

    fix

--- resources/views/layouts/mobile.blade.php
+++ resources/views/layouts/mobile.blade.php
@@ -140,7 +140,7 @@
                 <select class="selectpicker text-uppercase" name="types[]" title="WHICH PART WOULD YOU LIKE TO ENQUIRE" multiple>
                     <option value="membership">membership</option>
                     <option value="academy">academy</option>
-                    <option value="membership&activities">ACTIVITIES</option>
+                    <option value="membership & activities">ACTIVITIES</option>
                     <option value="tournament">tournament</option>
                     <option value="golf & cocktail bar">golf & cocktail bar</option>
                 </select>
@@ -289,8 +289,8 @@
                     <span class="fa fa-bars" aria-hidden="true"></span>
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="dLabel">
-                    <li><a href="cocktail" rel="external">GOLF&COCKTAIL BAR</a></li>
-                    <li><a href="coming_events" rel="external">MEMBERSHIP&ACTIVITIES</a></li>
+                    <li><a href="cocktail" rel="external">GOLF & COCKTAIL BAR</a></li>
+                    <li><a href="coming_events" rel="external">MEMBERSHIP & ACTIVITIES</a></li>
                     <li><a href="academy" rel="external">ACADEMY</a></li>
                     <li><a href="tournament" rel="external">TOURNAMENT</a></li>
                     <li><a href="price" rel="external">PRICING</a></li>
