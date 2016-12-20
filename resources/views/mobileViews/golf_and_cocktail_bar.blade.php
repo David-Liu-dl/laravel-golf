75e34500da68556769231e3b177dae25e2548804
Henry Heguang Miao <hikuimiao@gmail.com>
2016-12-21 00:02:24 +1100

    Convert image to text on the mobile site.

--- resources/views/mobileViews/golf_and_cocktail_bar.blade.php
+++ resources/views/mobileViews/golf_and_cocktail_bar.blade.php
@@ -34,6 +34,10 @@
             border-bottom: 1px dashed #cfb154;
         }
 
+        .img-block {
+            margin-top: 34px;
+        }
+
         .gold-btn{
             padding: 5px 10px;
             font-size: 13px;
@@ -51,15 +55,119 @@
             font-weight: normal;
             border-bottom: 1px solid #cfb154;
         }
+
+        .events-section {
+            padding: 40px 10px 40px 10px;
+            border-bottom: 2px dotted #cfb154;
+            font-family: "work-sans-light";
+            font-size: 13px;
+            color: rgba(255,255,255, 0.7);
+            line-height: 1.7em;
+        }
+
+        .hours-section .hours {
+            padding: 20px 5px 40px 5px;
+        }
+
+        .hours-section .day-of-week{
+            float: left;
+            width: 60%;
+        }
+
+        .hours-section .time{
+            float: left;
+            width: 40%;
+        }
+
+        #business-hours .label {
+            display: block;
+            width: 200px;
+            height: 50px;
+            margin: 0 auto;
+            margin-bottom: 10px;
+            line-height: 50px; 
+            background-color: #cfb154;
+            color: black;
+            font-family: "work-sans-bold";
+            font-size: 1em;
+            text-transform: uppercase;
+            border-radius: 0;
+            padding-top: 0px;
+            padding-bottom: 0px;
+        }
+
+
     </style>
 
     <div id="fixed-frame" class="container">
         <div id="events_block_title">
             <span style="text-transform: uppercase;font-family: work-sans-regular">golf&cocktail bar</span>
         </div>
+
+        <div id="events_introduction" class="events-section">
+            <p>
+                GPlus's breathtaking array of premium cocktails and spirits is derived from the best menus in the world with a slight twist. Each item on our menu is infused with golf terminology to get you and your colleagues into the swing of things. Furthermore, there is a drink on the menu to suit every individual much like the game of golf requires all types to enjoy our historic game.
+            </p>
+            <p>
+                The game of golf is to be enjoyed with friends over the finest quality beverages as per the games humble beginnings all those years ago. Staff from GPlus pride themselves on attention to detail in their mixology and have vast knowledge in the game of golf to ensure you and your colleagues have a night to remember.
+            </p>
+        </div>
+
+        <div id="business-hours" class="events-section">
+            <div class="hours-section">
+                <div>
+                    <span class="label">Open Hours</span>
+                </div>
+
+                <div class="hours clearfix">
+                    <div class="day-of-week">
+                        <p>MONDAY-WEDNESDAY</p>
+                        <p>THURSDAY-SATURDAY</p>
+                        <p>SUNDAY</p>
+                    </div>
+                    <div class="time">
+                        <p>12:00pm-9:00pm</p>
+                        <p>12:00pm-Late</p>
+                        <p>Booking Only</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="hours-section">
+                <div>
+                    <span class="label">Practice Hours</span>
+                </div>
+                <div class="hours clearfix">
+                    <div class="day-of-week">
+                        <p>MONDAY-WEDNESDAY</p>
+                        <p>THURSDAY-FRIDAY</p>
+                    </div>
+                    <div class="time">
+                        <p>12:00pm-6:00pm</p>
+                        <p>12:00pm-4:00pm</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="hours-section">
+                <div>
+                    <span class="label">Happy Hours</span>
+                </div>
+                <div class="hours clearfix">
+                    <div class="day-of-week">
+                        <p>MONDAY-WEDNESDAY</p>
+                        <p>THURSDAY-FRIDAY</p>
+                    </div>
+                    <div class="time">
+                        <p>4:30pm-6:30pm</p>
+                        <p>5:00pm-6:00pm</p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
         <div class="img-block">
-            <a href="#"><img class="golf-img" src="static/images/cocktail_bar-1_mobile.png" style="width:100%; height:auto"></a>
-            <a href="#"><img class="golf-img" src="static/images/cocktail_bar-2_mobile.png" style="width:100%; height:auto"></a>
             <a href="static/images/price_drink.jpg" rel="external"><img class="golf-img" src="static/images/price_drink.jpg" width="100%"></a>
             <a href="static/images/price_food.jpg" rel="external"><img class="golf-img" src="static/images/price_food.jpg" width="100%"></a>
 
